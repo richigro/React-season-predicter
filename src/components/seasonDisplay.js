5adc99cf24f879b66b17b7be59b491d2d6f9ed1c
Ricardo Guerrero <richi_gro@hotmail.com>
2019-04-02 22:35:18 -0500

    changes app functional component to class component to initiate state and have bee able to rerender when users location changes

---
+++ src/components/seasonDisplay.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function SeasonDisplay(props) {
+    return(
+        <div>
+            <h1>Howdy Folks!</h1>
+        </div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './components/seasonDisplay';
 
-function App(props){
-    return (
-        <div>
-            <h1>Hello</h1>
-        </div>
-    );   
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            lat: null
+        };
+        window.navigator.geolocation.getCurrentPosition(
+            (position) =>{
+                this.setState({ lat: position.coords.latitude});
+            },
+            (err) => console.log(err)
+        );
+    }
+
+
+    render() {
+        return (
+            <div>
+                
+            <SeasonDisplay />
+            <h1>Latitude: {this.state.lat}</h1>
+            </div>
+        ); 
+    }  
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
